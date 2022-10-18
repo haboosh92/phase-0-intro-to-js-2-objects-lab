a61433b8b884d5fe856116e0a755ef42c7c0e328
Hiba <eng.lilil92@gmail.com>
2022-10-19 02:12:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var employee = {
+    name : "Hiba" ,
+    streetAddress : "Al-Maghreb st."
+}
+function updateEmployeeWithKeyAndValue(object , key , value){
+    return{
+        ...object,
+        [key]:value,
+    }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(object , key , value) {
+   
+        object[key]= value;
+        return object;
+    
+}
+
+function destructivelyDeleteFromEmployeeByKey(object , key) {
+    
+        delete object[key];
+        return object;
+    
+}
+function deleteFromEmployeeByKey(object,key){
+    const newObject = {...object};
+    delete newObject[key];
+    return newObject;
+}
